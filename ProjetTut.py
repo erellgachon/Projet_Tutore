f40e8d2b724a03590dadd9b788ac6cfc9627ed1b
Erell Gachon <erell.gachon@etu.u-bordeaux.fr>
2021-02-23 19:01:26 +0100

    Sens indirect bijection

--- ProjetTut.py
+++ ProjetTut.py
@@ -1,7 +1,7 @@
 from tkinter import *
 #Sens direct de la bijection entre une permutation et deux tableaux de Young
 
-def permutationToYoung(tab) :
+def PermutationToYoung(tab) :
     P=[[]]
     Q=[[]]
     for i in range(len(tab)) :
@@ -11,8 +11,41 @@ def permutationToYoung(tab) :
         Q[x].append(i+1)
     return [P,Q]
 
+def FindX(tab,X) :
+    for i in range(len(tab)) :
+        for j in range(len(tab[i])) :
+            if tab[i][j]==X :
+                return i,j
+    return -1,-1
 
+def YoungToPermutation(P,Q,n) :
+    permutation=[]
+    for x in range(n,0,-1) :
+        i,j=FindX(Q,x)
+        print(i,j)
+        elt=insertInv(P,i,j)
+        if (len(P[i])==0) :
+            P.remove([])
+        permutation.insert(0,elt)
+        print("Permutation",permutation)
+        print("P",P)
+        print("Q",Q)
+    return permutation
 
+def insertInv(P,i,j) :
+    elt=P[i][j]
+    P[i].remove(elt)
+    i1=i-1
+    while (i1>=0):
+        j1=len(P[i1])-1
+        while (j1>=0 and P[i1][j1]>=elt) :
+            j1=j1-1
+        tmp=P[i1][j1]
+        P[i1][j1]=elt
+        elt=tmp
+        i1=i1-1
+    return elt
+    
 def insert(elmt,tab,i=0) :
     j=0
     if (i==len(tab)) :
@@ -46,7 +79,7 @@ def RS() :
 
 
 
-    [P,Q]=permutationToYoung(permut)  #Tab de Young
+    [P,Q]=PermutationToYoung(permut)  #Tab de Young
     for j in range(len(P)) :
         y=0 #affichage
         for i in range(0,len(P[j])) :
